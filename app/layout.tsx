f6c8dd34a91d19a238e0efa4d67ff68671acf98f
jbderiada <jbderiada1@gmail.com>
2025-10-22 22:42:16 +0800

    Working SearchBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import "./globals.css";
 import { Inter } from 'next/font/google';
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,7 +28,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body className={cn("bg-secondary",inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="sytem" enableSystem disableTransitionOnChange>
             {children}
           </ThemeProvider>
